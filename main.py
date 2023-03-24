ab3d777f9eeb21576cf3107dc587c6aafcc8c5f8
qtrugle0113 <rikateamovt1@gmail.com>
2023-03-25 08:17:59 +0900

    test1

--- main.py
+++ main.py
@@ -27,12 +27,12 @@ from datetime import date, timedelta  # datetime
 '''
 
 import kivy
-from kivy import Config
+#from kivy import Config
 from kivy.app import App
 from kivy.uix.screenmanager import Screen, ScreenManager
 
-Config.set('graphics', 'width', '450')
-Config.set('graphics', 'height', '1000')
+#Config.set('graphics', 'width', '450')
+#Config.set('graphics', 'height', '1000')
 
 class WindowManager(ScreenManager):
     pass
