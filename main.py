6002ed9cad6ecf291b1108e1b6dad661196e0f3c
qtrugle0113 <rikateamovt1@gmail.com>
2023-03-25 11:58:00 +0900

    test5

--- main.py
+++ main.py
@@ -43,9 +43,9 @@ from kivy.uix.screenmanager import Screen, ScreenManager
 # Config.set('graphics', 'height', '1000')
 
 def today_question():
-    #questions = pd.read_csv('data/questions_list.csv')
-    #ques_id = random.randint(0, len(questions) - 1) + 1
-    #today_ques = questions.loc[ques_id - 1, 'english']
+    # questions = pd.read_csv('data/questions_list.csv')
+    # ques_id = random.randint(0, len(questions) - 1) + 1
+    # today_ques = questions.loc[ques_id - 1, 'english']
     ques_id = random.randint(0, 10) + 1
     today_ques = str(ques_id)
     return today_ques, ques_id
@@ -133,6 +133,7 @@ class QnAWindow(Screen):
             self.mood = 'happy'
 
     question, ques_id = today_question()
+
 
 class SettingWindow(Screen):
     pass
